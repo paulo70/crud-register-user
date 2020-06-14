3f2648c2357745b1988771d12c1e37ee531502b1
paulo70 <paulofrontend@gmail.com>
2020-06-14 16:38:12 -0300

    set initial state to form fn save to clear my form fn save to change or save user

--- frontend/src/components/user/userCrud.jsx
+++ frontend/src/components/user/userCrud.jsx
@@ -7,7 +7,42 @@ const headerProps = {
   subtitle: 'Register User: Add, List, Change and Remove'
 }
 
+const baseURL = 'http://localhost:3001/users'
+
 class User extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      name: '',
+      email: '',
+      list: []
+    }
+  }
+
+  clear(){
+    this.setState({ user: this.state.user })
+  }
+
+  save(){
+    const user   = this.state.user
+    const method = user.id ? 'put' : 'post'
+    const url    = user.id ? `${baseURL}/${user.id}` : baseURL
+
+    axios[method](url, user)
+      .then(resp => {
+        const list = this.getUpdatedList(resp.data)
+        this.setState({ user: this.state.user, list })
+      })
+  }
+
+  getUpdatedList(user){
+    const list = this.state.list.filter(u => u.id !== user.id)
+    list.unshift(user)
+
+    return list
+  }
 
   render(){
     return (
